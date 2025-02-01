c545bf1709a6983269890b2e7792e03b695a043b
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-01 08:24:27 +0000

    ft_atoi

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -22,9 +22,24 @@ int	ft_atoi(char *str)
 	return (result * sign);
 }
 #include <stdio.h>
-int	main(int argc, char **argv)
+int	main()
 {
-	if (argc == 2)
-		printf("%i\n", ft_atoi(argv[1]));
+	// 基本使用示例
+	char	*str1 = "123";
+	int		num1 = ft_atoi(str1);
+	printf("转换 \"%s\" 得到：%d\n", str1, num1);
+	// 处理带符号的数字
+	char	*str2 = "-456";
+	int		num2 = ft_atoi(str2);
+	printf("转换 \"%s\" 得到：%d\n", str2, num2);
+	// 处理带空格的数字
+	char	*str3 = "   789";
+	int		num3 = ft_atoi(str3);
+	printf("转换 \"%s\" 得到：%d\n", str3, num3);
+	// 处理非法输入
+	char	*str4 = "12abc34";
+	int		num4 = ft_atoi(str4);
+	printf("转换 \"%s\" 得到：%d（只转换到非数字字符前）\n", str4, num4);
+
 	return (0);
 }
